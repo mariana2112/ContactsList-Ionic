d16abe7d5aa66b6b852c2b7babb8a810e27cb2e4
mariana2112 <marianaroveferreira@gmail.com>
2023-03-30 12:31:55 -0300

    start

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+  standalone: true,
+  imports: [IonicModule],
+})
+export class HomePage {
+  constructor(private router: Router) {}
+
+  pagRoute(route:string){
+    this.router.navigate([route])
+  }
+}
+
